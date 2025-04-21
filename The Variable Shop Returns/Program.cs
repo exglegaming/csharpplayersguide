476b8aad51dece3be1304b9f43a6a3c620147827
ExgleGaming <167655468+ExgleGaming@users.noreply.github.com>
2025-04-20 21:14:58 -0400

    the variable shop returns complete

---
+++ The Variable Shop Returns/Program.cs
@@ -0,0 +1,47 @@
+﻿// all 14 types of variables
+byte a = 1;
+short b = 2;
+int c = 3;
+long d = 4;
+sbyte e = 5;
+ushort f = 6;
+uint g = 7;
+ulong h = 8;
+char i = 'a';
+string j = "Hello World!";
+float k = 1.234f;
+double l = 1.234;
+decimal m = 1.234m;
+bool n = true;
+
+// change the values of the variables
+a = 10;
+b = 20;
+c = 30;
+d = 40;
+e = 50;
+f = 60;
+g = 70;
+h = 80;
+i = 'b';
+j = "Goodbye World!";
+k = 2.3f;
+l = 2.345;
+m = 2.345m;
+n = false;
+
+// print all 14 variables
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
+Console.WriteLine(m);
+Console.WriteLine(n);
